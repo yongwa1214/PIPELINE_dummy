7d09ca7aef8c3dedc9c8a3e17131eb815753c985
yongwa1214 <yongwa1214@gmail.com>
2025-11-24 17:07:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/green/pipeline_dummy/PipelineDummyApplication.java
@@ -0,0 +1,13 @@
+package com.green.pipeline_dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineDummyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineDummyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/pipeline_dummy/PipelineDummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.pipeline_dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDummyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
